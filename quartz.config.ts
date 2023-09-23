c9f5cf1d22ddfb87dc6d24a9a9d0f215b3764b44
Elijah <elijahshellsanchez@outlook.com>
2023-09-23 15:15:25 +0200

    Quartz sync: Sep 23, 2023, 3:15 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -7,7 +7,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "elahi-cs.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "published",
     theme: {
